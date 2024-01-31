fd46d1e643ec77f88dc516a03e68e3040411202e
SunLiveMatrix <152664029+SunLiveMatrix@users.noreply.github.com>
2024-01-31 20:36:42 -0300

    mct
    Codux is an all-in-one visual development environment. Whether you’re starting a new app, creating components, or editing an existing project, every visual change you make reflects instantly in the code and vice versa. To learn more about Codux, visit our website - [https://www.codux.com/](https://www.codux.com/)

---
+++ matrix/gnu/bin/mct.d
@@ -0,0 +1,23 @@
+module matrix.gnu.mct;
+
+import std.stdio;
+import std.stdint;
+import std.string;
+
+export class MCTMatrix {
+    enum MyMCTType
+    {
+        init,
+    }
+}
+
+export class MCTMatrixType {
+    enum EnumValue = 512;    
+}
+
+export class MCTMatrixEvent {
+    void mct(){
+        return mct();
+    }
+}
+
